14f34dab0c82deb58039ba4b383f421c7cfb4f0a
Judith Landeros <judith.lt@tecjerez.edu.mx>
2024-05-30 00:14:05 -0600

    1.Creacion de package

---
+++ src/conexionBD/ConexionBD.java
@@ -0,0 +1,79 @@
+package conexionBD;
+
+import java.sql.*;
+
+public class ConexionBD {
+    private Connection conexion;
+    private PreparedStatement preparedStatement;
+    //private Statement statement;
+    /* NOTA: Es preferible utilizar PreparedStatement para evitar
+         SQL INJECTION
+     */
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    private ResultSet resultSet;
+
+    public ConexionBD(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+
+            String URL = "jdbc:mysql://localhost:3306/autosamistosos"; //direccion de a que se conecta a mysql
+            conexion = DriverManager.getConnection(URL,  "root",  "judilth@3");
+
+            System.out.println("Yeeeeiiiii ya casi soy ISC");
+
+        }catch(ClassNotFoundException e){
+            // throw new RuntimeException(e);
+            System.out.println("Error en el DRIVER");
+        } catch (SQLException e) {
+            System.out.println("Error en la URL");
+        }
+
+    }
+    //Metodos para operaciones ABCC (ALTAS, BAJAS, CAMBIOS, CONSULTAS )
+    //(DataManipulation Languaje)
+    public boolean ejecutarInstruccionDML(String instruccionSQL) throws SQLException {
+        try {
+            preparedStatement = conexion.prepareStatement(instruccionSQL);
+            return preparedStatement.executeUpdate() >= 1;
+        } finally {
+            cerrarRecursos();
+        }
+    }
+
+    //metodo para consultas
+    public ResultSet ejecutarConsultaSQL(String instruccionSQL) throws SQLException {
+        try {
+            preparedStatement = conexion.prepareStatement(instruccionSQL);
+            resultSet = preparedStatement.executeQuery();
+            return resultSet;
+        } finally {
+
+        }
+    }
+    private void cerrarRecursos() {
+        try {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (conexion != null) {
+                conexion.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al cerrar los recursos de la base de datos");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new ConexionBD();
+
+    }
+}//class
