3487b52736179ffcebb2856decdee8f3b658b9b6
Judith Landeros <judith.lt@tecjerez.edu.mx>
2024-05-30 01:22:27 -0600

    6.Modificacion de class AutomovilDAO

--- src/Controlador/AutomovilDAO.java
+++ src/Controlador/AutomovilDAO.java
@@ -1,91 +1,98 @@
 package Controlador;
 
 import conexionBD.ConexionBD;
+import modelo.Automovil;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AutomovilDAO {
      ConexionBD conexion = new ConexionBD();
 
-        public void agregarAutomovil () {
-            try {
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Ingrese el modelo:");
-                String modelo = scanner.nextLine();
-                System.out.println("Ingrese el precio de lista:");
-                double precioLista = scanner.nextDouble();
-                System.out.println("Ingrese la fecha de fabricación (AAAA-MM-DD):");
-                String fechaFabricacion = scanner.next();
-                System.out.println("Ingrese la placa:");
-                String placa = scanner.next();
-                System.out.println("Ingrese el ID del cliente:");
-                String idCliente = scanner.next();
-                System.out.println("Ingrese el ID de la marca:");
-                String idMarca = scanner.next();
+    public void agregarAutomovil(String modelo, double precioLista, String fechaFabricacion, String placa, String idCliente, String idMarca) {
+        try {
+            String sql = "INSERT INTO automovil (modelo, precio_lista, fecha_fabricacion, placa, Cliente_idCliente, Marca_idMarca) " +
+                    "VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement statement = conexion.getConexion().prepareStatement(sql);
+            statement.setString(1, modelo);
+            statement.setDouble(2, precioLista);
+            statement.setString(3, fechaFabricacion);
+            statement.setString(4, placa);
+            statement.setString(5, idCliente);
+            statement.setString(6, idMarca);
+            int filasAfectadas = statement.executeUpdate();
 
-                String sql = "INSERT INTO automovil (modelo, precio_lista, fecha_fabricacion, placa, Cliente_idCliente, Marca_idMarca) " +
-                        "VALUES (?, ?, ?, ?, ?, ?)";
-                PreparedStatement statement = conexion.getConexion().prepareStatement(sql);
-                statement.setString(1, modelo);
-                statement.setDouble(2, precioLista);
-                statement.setString(3, fechaFabricacion);
-                statement.setString(4, placa);
-                statement.setString(5, idCliente);
-                statement.setString(6, idMarca);
-                int filasAfectadas = statement.executeUpdate();
-
-                if (filasAfectadas > 0) {
-                    System.out.println("Automóvil agregado correctamente.");
-                } else {
-                    System.out.println("Error al agregar el automóvil.");
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
+            if (filasAfectadas > 0) {
+                System.out.println("Automóvil agregado correctamente.");
+            } else {
+                System.out.println("Error al agregar el automóvil.");
             }
-        }
-
-        public void eliminarAutomovil () {
-            try {
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Ingrese la placa del automóvil que desea eliminar:");
-                String placa = scanner.next();
-
-                String sql = "DELETE FROM automovil WHERE placa = ?";
-                PreparedStatement statement = conexion.getConexion().prepareStatement(sql);
-                statement.setString(1, placa);
-                int filasAfectadas = statement.executeUpdate();
-
-                if (filasAfectadas > 0) {
-                    System.out.println("Automóvil eliminado correctamente.");
-                } else {
-                    System.out.println("No se encontró ningún automóvil con la placa especificada.");
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public void actualizarAutomovil () {
-
-        }
-
-        public void consultarAutomoviles () {
-            try {
-                Statement statement = conexion.getConexion().createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM automovil");
-
-                while (resultSet.next()) {
-                    System.out.println("Modelo: " + resultSet.getString("modelo"));
-                    System.out.println("Precio de lista: " + resultSet.getDouble("precio_lista"));
-                    System.out.println("Fecha de fabricación: " + resultSet.getDate("fecha_fabricacion"));
-                    System.out.println("Placa: " + resultSet.getString("placa"));
-                    System.out.println("ID del cliente: " + resultSet.getString("Cliente_idCliente"));
-                    System.out.println("ID de la marca: " + resultSet.getString("Marca_idMarca"));
-                    System.out.println();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
+
+    public void eliminarAutomovil(String placa) {
+        try {
+            String sql = "DELETE FROM automovil WHERE placa = ?";
+            PreparedStatement statement = conexion.getConexion().prepareStatement(sql);
+            statement.setString(1, placa);
+            int filasAfectadas = statement.executeUpdate();
+
+            if (filasAfectadas > 0) {
+                System.out.println("Automóvil eliminado correctamente.");
+            } else {
+                System.out.println("No se encontró ningún automóvil con la placa especificada.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void actualizarAutomovil(String placa, String nuevoModelo, double nuevoPrecioLista) {
+        try {
+            String sql = "UPDATE automovil SET modelo = ?, precio_lista = ? WHERE placa = ?";
+            PreparedStatement statement = conexion.getConexion().prepareStatement(sql);
+            statement.setString(1, nuevoModelo);
+            statement.setDouble(2, nuevoPrecioLista);
+            statement.setString(3, placa);
+
+            int filasAfectadas = statement.executeUpdate();
+
+            if (filasAfectadas > 0) {
+                System.out.println("Automóvil actualizado correctamente.");
+            } else {
+                System.out.println("No se encontró ningún automóvil con la placa especificada.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public ArrayList<Automovil> consultarAutomoviles() {
+        ArrayList<Automovil> listaAutomoviles = new ArrayList<>();
+        try {
+            Statement statement = conexion.getConexion().createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM automovil");
+
+            while (resultSet.next()) {
+                String idAutomovil = resultSet.getString("idAutomovil");
+                String modelo = resultSet.getString("modelo");
+                double precioLista = resultSet.getDouble("precio_lista");
+                String fechaFabricacion = resultSet.getString("fecha_fabricacion");
+                String placa = resultSet.getString("placa");
+                String clienteId = resultSet.getString("Cliente_idCliente");
+                String marcaId = resultSet.getString("Marca_idMarca");
+
+                Automovil automovil = new Automovil(idAutomovil, modelo, precioLista, fechaFabricacion, clienteId, placa, marcaId);
+                listaAutomoviles.add(automovil);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return listaAutomoviles;
+    }
+
+}
