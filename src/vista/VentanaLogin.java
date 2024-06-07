7932fa1c4ffc99f1f1e23bebd14644b9bf7d7ae7
Judith Landeros <judith.lt@tecjerez.edu.mx>
2024-06-06 21:50:31 -0600

    15.Diseño de ventana login

--- src/vista/VentanaLogin.java
+++ src/vista/VentanaLogin.java
@@ -5,6 +5,10 @@ import java.awt.*;
 
 public class VentanaLogin extends JFrame {
     private JPanel panelLogin;
+    private JTextField txtusuario;
+    private JPasswordField password;
+    private JLabel lblUsuario, lblTitulo;
+    private JLabel lblpassword;
 
 
     public VentanaLogin(){
@@ -13,10 +17,28 @@ public class VentanaLogin extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panelLogin = new JPanel();
         panelLogin.setLayout(null);
-
-        add(panelLogin);
-        setVisible(true);
         setLayout(new BorderLayout());
+        add(panelLogin);
+
+        lblTitulo = new JLabel("INICIO SESION");
+        lblTitulo.setBounds(100,20,100,25);
+        panelLogin.add(lblTitulo);
+
+        lblUsuario = new JLabel("USUARIO");
+        lblUsuario.setBounds(50, 50, 80, 25);
+        panelLogin.add(lblUsuario);
+        txtusuario = new JTextField(20);
+        txtusuario.setBounds(140, 50, 100, 25);
+        panelLogin.add(txtusuario);
+
+        lblpassword = new JLabel("CONTRASEÑA");
+        lblpassword.setBounds(50, 90, 80, 25);
+        panelLogin.add(lblpassword);
+        password = new JPasswordField(20);
+        password.setBounds(140, 90, 100, 25);
+        panelLogin.add(password);
+
+        setVisible(true);
     }
 
     public static void main(String[] args) {
