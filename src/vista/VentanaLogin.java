91bbdaf9facaf73d2ddb1510fb726ff881f19e6c
Judith Landeros <judith.lt@tecjerez.edu.mx>
2024-06-06 21:33:19 -0600

    14.Visibilidad a ventana login

--- src/vista/VentanaLogin.java
+++ src/vista/VentanaLogin.java
@@ -1,8 +1,9 @@
 package vista;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class VentanaLogin extends JInternalFrame {
+public class VentanaLogin extends JFrame {
     private JPanel panelLogin;
 
 
@@ -10,11 +11,12 @@ public class VentanaLogin extends JInternalFrame {
         setTitle("VENTANA LOGIN");
         setSize(400, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         panelLogin = new JPanel();
         panelLogin.setLayout(null);
 
-
+        add(panelLogin);
+        setVisible(true);
+        setLayout(new BorderLayout());
     }
 
     public static void main(String[] args) {
