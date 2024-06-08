d75dacd84d4d1c283f29353dedaa7a54c89d95ec
Judith Landeros <judith.lt@tecjerez.edu.mx>
2024-06-07 23:31:49 -0600

    20.Clases en Main para poder generar .JAR

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
+import Controlador.AutomovilDAO;
+import conexionBD.ConexionBD;
+import vista.VentanaLogin;
+
 public class Main {
+
+    ConexionBD conexion = ConexionBD.getConexionInstancia();
+    AutomovilDAO automovilDAO= new AutomovilDAO();
+
+    VentanaLogin ventanaLogin = new VentanaLogin();
+
+
 }
