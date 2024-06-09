fd0e5e821cb8e555334e7e2e1dc30bcf976f491a
Judith Landeros <judith.lt@tecjerez.edu.mx>
2024-06-08 23:05:46 -0600

    25.Haber agregado elementos a ventana cambios

--- src/vista/VentanaCambios.java
+++ src/vista/VentanaCambios.java
@@ -5,7 +5,24 @@ import java.awt.*;
 
 public class VentanaCambios extends JInternalFrame {
 
-    public VentanaCambios(){
+        private JButton btm_Modificar = new JButton("MODIFICAR");
+        private JButton btm_salir = new JButton("SALIR");
+        private JLabel btm_marca = new JLabel("MARCA");
+        private JLabel btm_idModelo = new JLabel("MODELO");
+        private JLabel btm_idCalcomania = new JLabel("IDAUTOMOVIL");
+        private JLabel btm_fechafab = new JLabel("FECHA FABRICACION");
+        private JLabel btm_precio = new JLabel("PRECIO");
+        private JTextField Marca_idMarca = new JTextField();
+        private JTextField modelo = new JTextField();
+        private JTextField idAutomovil = new JTextField();
+        private JTextField fecha_fabricacion = new JTextField();
+        private JTextField precio_lista = new JTextField();
+        private JLabel btm_placa = new JLabel("PLACA");
+        private JTextField placa = new JTextField();
+        private JLabel btm_Cliente = new JLabel("CLIENTE");
+        private JComboBox<String> Cliente_idCliente = new JComboBox<>(new String[]{"C001", "C002", "C003", "C004", "C005", "C006", "C007"});
+
+        public VentanaCambios(){
 
             setTitle("Cambios");
             setSize(600, 400);
