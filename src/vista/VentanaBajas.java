57f6cee50f9621ac491c7a8cbd5369562722a686
Judith Landeros <judith.lt@tecjerez.edu.mx>
2024-06-08 23:10:40 -0600

    26.Declaracion de elementos de ventana bajas

--- src/vista/VentanaBajas.java
+++ src/vista/VentanaBajas.java
@@ -5,6 +5,16 @@ import java.awt.*;
 
 public class VentanaBajas extends JInternalFrame {
 
+    private JButton btmSalir;
+    private JLabel btmPlaca;
+    private JLabel btmidAutomovil;
+    private JTextField placa;
+    private JTextField idAutomovil;
+    private JButton btm_Eliminar;
+    private JTable tabla_Contenido;
+    private JButton buscar;
+    private JButton btnRespaldo;
+
     public VentanaBajas() {
         setTitle("Bajas");
         setSize(300, 200);
