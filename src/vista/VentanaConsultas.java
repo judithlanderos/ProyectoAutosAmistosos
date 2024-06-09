4aacec7361df77c87af530b6a5bf6f24f5d204a3
Judith Landeros <judith.lt@tecjerez.edu.mx>
2024-06-08 21:20:28 -0600

    22.Implementacion de ActionListener para agregar eventos a consultas

--- src/vista/VentanaConsultas.java
+++ src/vista/VentanaConsultas.java
@@ -1,11 +1,26 @@
 package vista;
 
+import conexionBD.ConexionBD;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class VentanaConsultas extends JInternalFrame {
+public class VentanaConsultas extends JInternalFrame  implements ActionListener {
+
+    private final ConexionBD miConexion;
+    private JTextField txtIdAutomovil;
+    private JTextField txtModelo;
+    private JTextField txtFechaFabricacion;
+    private JTextField txtPlaca;
+    private JTextField txtMarcaIdMarca;
+    private JTable tablaContenido;
+    private JRadioButton radioIdAutomovil, radioModelo, radioFechaFabricacion, radioPlaca, radioMarcaIdMarca;
+    private JButton btnBuscar, btnRestablecer;
 
     public VentanaConsultas(){
+        miConexion = ConexionBD.getConexionInstancia();
 
         setTitle("Consultas");
         setSize(300, 200);
@@ -16,7 +31,45 @@ public class VentanaConsultas extends JInternalFrame {
         setBackground(Color.yellow);
         setLayout(new BorderLayout());
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(204, 204, 255));
+        panel.setLayout(new GridLayout(9, 2, 5, 5));
+
+        radioIdAutomovil = new JRadioButton("ID AUTOMOVIL");
+        txtIdAutomovil = new JTextField();
+
+        radioModelo = new JRadioButton("MODELO");
+        txtModelo = new JTextField();
+
+        radioFechaFabricacion = new JRadioButton("FECHA");
+        txtFechaFabricacion = new JTextField();
+
+        radioPlaca = new JRadioButton("PLACA");
+        txtPlaca = new JTextField();
+
+        radioMarcaIdMarca = new JRadioButton("ID MARCA");
+        txtMarcaIdMarca = new JTextField();
+
+        btnBuscar = new JButton("BUSCAR");
+        add(btnBuscar);
+
+        btnRestablecer = new JButton("RESTABLECER");
+
+
         setVisible(true);
+
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new VentanaConsultas().setVisible(true);
+            }
+        });
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
